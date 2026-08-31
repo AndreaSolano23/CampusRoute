16623873d5bd1a8a3366e535ce714863415c4e7c
Paula Andrea Solano <paula.solano-m@mail.escuelaing.edu.co>
2026-08-31 09:33:47 -0500

    feature: pruebas tdd

--- src/test/java/edu/eci/dosw/AppTest.java
+++ src/test/java/edu/eci/dosw/AppTest.java
@@ -1,20 +1,44 @@
 package edu.eci.dosw;
 
-import static org.junit.Assert.assertTrue;
+//import edu.eci.dosw.model.Route;
+//import edu.eci.dosw.model.Segment;
+//import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import java.util.List;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouteTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void shouldDetectBlockedRoute() {
+        Segment segment = new Segment(
+                "A", "B", 100, 2, 4, false, false
+        );
+
+        Route route = new Route("Ruta bloqueada", List.of(segment));
+
+        assertTrue(route.isBlocked());
     }
+
+    @Test
+    void shouldCalculateTotalTime() {
+        Segment s1 = new Segment("A", "B", 100, 5, 4, false, true);
+        Segment s2 = new Segment("B", "C", 200, 3, 4, false, true);
+
+        Route route = new Route("Ruta", List.of(s1, s2));
+
+        assertEquals(8, route.totalTimeMinutes());
+    }
+
+    @Test
+    void shouldCalculateTotalDistance() {
+        Segment s1 = new Segment("A", "B", 100, 5, 4, false, true);
+        Segment s2 = new Segment("B", "C", 200, 3, 4, false, true);
+
+        Route route = new Route("Ruta", List.of(s1, s2));
+
+        assertEquals(300, route.totalDistanceMeters());
+    }
+
 }
